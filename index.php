34d8b831417bdb8216a1fd82f6933cf5fc295e3b
hossain377 <hossainahmed377@gmail.com>
2021-01-09 08:41:22 +0600

    comparism & logical operator

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 <?php
+//comparism operator, conditional operator
+$a=20;
+$b="20";
+$age=28;
+echo $a!==$b . '<br>';
+if(!($age>=26)){
+    echo ' this is the valid value';
+}
 
-
-//arithmatics
-$a=10;
-$b=4;
-
-//$c=$a%$b;
-
-$a++;
-++$a;
-echo $a . '<br>'; 
-/*========== Assignment operator=====
-x+=y -->x=x+y
-
-+=,*=,
-*/
-
-$a/=$b;
-echo $a;
-
+ 
 ?>
