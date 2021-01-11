87436f341996bc98f0525528c5518a9de4194f22
hossain377 <hossainahmed377@gmail.com>
2021-01-11 09:06:09 +0600

    20-Do while

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
 
-$s="hello";
-$s .= " this is";
-$s .= " the world";
-echo $s . "<br>";
 
 $a=1;
-echo "<ul>";
-while($a<=20){
-    echo "<li>" . $a . ") hello yahoo baba </li>";
-    $a=$a+2;
-}
-echo "<ul>";    
+
+do{
+    echo $a . ") yahoo baba" . "<br>";
+    $a++;
+}while($a<=10)
     
     
 ?>
