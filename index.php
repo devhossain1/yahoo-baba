5c213ad3536573a6062c38a7058bc8788eea3b71
hossain377 <hossainahmed377@gmail.com>
2021-01-11 08:08:09 +0600

    18-string operator/19-while loop

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
-$x=20;
 
-$z ="value is : " . ($x>30?  "greater" : "smaller");
-echo $z;
+$s="hello";
+$s .= " this is";
+$s .= " the world";
+echo $s . "<br>";
 
-
+$a=1;
+echo "<ul>";
+while($a<=20){
+    echo "<li>" . $a . ") hello yahoo baba </li>";
+    $a=$a+2;
+}
+echo "<ul>";    
+    
+    
 ?>
