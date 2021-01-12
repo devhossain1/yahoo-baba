dde14b68ad771c8fccac55a97b4103bdb69e378c
hossain377 <hossainahmed377@gmail.com>
2021-01-12 14:21:37 +0600

    23-continue/break, 24-goto

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
 
-for($a = 1; $a<=100; $a=$a+10){
-   for($b=$a; $b < $a+10; $b++){
-       echo $b . " ";
-   }
-    echo "<br>";
+for($a=1; $a<=10;$a++){
+    if($a==4){
+        echo "No. " . $a . " <br>";
+        continue;
+    }
+    
+    
+    echo "number " . $a . "<br>";
 }
-    
-    
+
+echo "Hello yahoo baba<br>";
+echo "Hello yahoo baba<br>";
+goto abc;
+echo "Hello yahoo baba<br>";
+echo "Hello yahoo baba<br>";
+echo "Hello yahoo baba<br>";
+echo "Hello yahoo baba<br>";
+
+abc:
+echo "Hey is the label abc";
+
 ?>
