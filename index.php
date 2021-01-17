52c118b58e1c16b70dae6a702eba410861eff31c
hossain377 <hossainahmed377@gmail.com>
2021-01-17 09:23:31 +0600

    29-variable function

--- index.php
+++ index.php
@@ -1,44 +1,10 @@
 <?php
 
-function sum($math, $eng, $sci){
-    $s= $math+$eng+$sci;
-    
-    return $s;
-     
-}
-function percentage($st){
-  $per = $st / 3; 
-    
-  echo $per . "%" . "<br>";
-}
+$sayHello = function($name){
+    echo "Hello $name";
+};
 
-$total=sum(34,50,100);
-percentage($total);
-
- //=====function refernce======
+$sayHello("yahoo baba");
 
 
-function testing(&$string){  
-  $string .= " and someting extra. <br>"; 
-}
-
-$str = "This is a string";
-testing($str);
-echo $str;
-
-//=====================
-function first($num){
-   $num +=5;    
-}
-
-function second(&$num){
-   $num +=8;  
-}
-
-$number = 10;
-first($number);
-echo "original value is $number<br>";
-
-second($number);
-echo "original value is $number<br>";
 ?>
