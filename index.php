0bba2fe06e924fe196f34a467955e3bc1d0d61b9
hossain377 <hossainahmed377@gmail.com>
2021-01-18 18:20:51 +0600

    30-recursive function

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
 
 $sayHello = function($name){
-    echo "Hello $name";
+    echo "Hello $name <br>";
 };
 
 $sayHello("yahoo baba");
 
+//recursive function
+
+function factorial($n){
+    if($n == 0){
+      return 1;
+    }else{
+      return ($n * factorial($n-1));   
+    }
+}
+echo factorial(7);
 
 ?>
