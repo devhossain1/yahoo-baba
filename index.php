817ca5310d6be66b87b2a00a697e9db78096e7f7
hossain377 <hossainahmed377@gmail.com>
2021-01-20 09:06:42 +0600

    33-assosiative array

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
-$colors=["red",20,"blue", "black"];
-echo "<ol>";
- for($i=0;$i < 4; $i++){
-     echo "<li> $colors[$i] <br>";
- }
- echo "</ol>"; 
+$age = [
+    100 => 25,
+    "bill" =>28.5,
+    32 =>22
+];
+
+echo "<pre>";
+var_dump($age);
+echo "</pre>";
+
+echo $age[100] . "<br>";
+echo $age["bill"] . "<br>";
+echo $age[32] . "<br>";
 
 ?>
