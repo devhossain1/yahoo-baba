eddeea0ece7fee0dc75f4b01f746e99ae4dddef0
hossain377 <hossainahmed377@gmail.com>
2021-01-26 11:38:01 +0600

    36-associative array

--- index.php
+++ index.php
@@ -1,28 +1,44 @@
 <?php
 
-$emp = [
-    [1,"krisna","Manager",50000],
-    [2,"salman","salesman",20000],
-    [3,"mohon","computer operator",21000],
-    [4,"amir","Driver",5000]
+$marks = [
+    "Krishana" =>[
+        "physics" =>85,
+        "math" =>78,
+        "chemistry" =>79
+    ],
+    "Salman" =>[
+        "physics" =>80,
+        "math" =>68,
+        "chemistry" =>70
+    ],
+    "Mohon" =>[
+        "physics" =>75,
+        "math" =>98,
+        "chemistry" =>75
+    ],
+    
 ];
 
-
-echo "<table border='2px' cellpadding='5px' cellspacing='0'>";
-echo "<tr>
-        <th>ID.</th>
-        <th>name</th>
-        <th>resignation</th>
-        <th>salarry</th>
-    </tr>";
-foreach($emp as $v1){
-    echo "<tr>";
-    foreach($v1 as $v2){
-        echo "<td> $v2 </td>";
-    }
+echo "<table border='2px' cellpadding='5px' cellspacing='0'>
+<tr>
+    <th>name</th>
+    <th>physics</th>
+    <th>maths</th>
+    <th>chemistry</th>
+    
+</tr>";
+foreach($marks as $key =>$v1){
+    echo "<tr>
+          <td>$key</td>";
+         foreach($v1 as $v2){
+             echo "<td> $v2 </td>"; 
+         }
     echo "</tr>";
 }
 echo "</table>";
 
+echo "<pre>";
+print_r($marks);
+echo "</pre>";
 
 ?>
