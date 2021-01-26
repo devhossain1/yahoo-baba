ac7516fb3ac09c5850ece102445fdc9e048b2108
hossain377 <hossainahmed377@gmail.com>
2021-01-26 08:37:42 +0600

    multidimentional array

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
 
-$age = [
-    "bill" =>25,
-    "steve " =>28,
-    "elon" =>20,
+$emp = [
+    [1,"krisna","Manager",50000],
+    [2,"salman","salesman",20000],
+    [3,"mohon","computer operator",21000],
+    [4,"amir","Driver",5000]
 ];
-echo "<ul>";
-foreach($age as $key => $value){
-    echo  "<li>$key = $value </li>";
+
+
+echo "<table border='2px' cellpadding='5px' cellspacing='0'>";
+echo "<tr>
+        <th>ID.</th>
+        <th>name</th>
+        <th>resignation</th>
+        <th>salarry</th>
+    </tr>";
+foreach($emp as $v1){
+    echo "<tr>";
+    foreach($v1 as $v2){
+        echo "<td> $v2 </td>";
+    }
+    echo "</tr>";
 }
-echo "</ul>";
+echo "</table>";
+
 
 ?>
