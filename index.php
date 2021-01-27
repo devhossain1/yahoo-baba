4055bd7499430800cef8245a88e0a5fd4fc0d95e
hossain377 <hossainahmed377@gmail.com>
2021-01-27 10:29:21 +0600

    foreach loop with list

--- index.php
+++ index.php
@@ -1,44 +1,57 @@
 <?php
 
-$marks = [
-    "Krishana" =>[
-        "physics" =>85,
-        "math" =>78,
-        "chemistry" =>79
+$emp = [
+    [1,"krisna","Manager",50000],
+    [2,"salman","salesman",20000],
+    [3,"mohon","computer operator",21000],
+    [4,"amir","Driver",5000]
+];
+
+$emp =[
+    [
+        "id" =>1,
+        "name" =>"krisna",
+        "designation" =>"manager",
+        "salary" =>50000
     ],
-    "Salman" =>[
-        "physics" =>80,
-        "math" =>68,
-        "chemistry" =>70
+     [
+        "id" =>2,
+        "name" =>"Salman",
+        "designation" =>"Salesman",
+        "salary" =>40000
     ],
-    "Mohon" =>[
-        "physics" =>75,
-        "math" =>98,
-        "chemistry" =>75
+     [
+        "id" =>3,
+        "name" =>"mohon",
+        "designation" => "computer operator",
+        "salary" =>35000
+    ],
+     [
+        "id" =>4,
+        "name" =>"amir",
+        "designation" =>"Driver",
+        "salary" =>5000
+    ],
+    [
+        "id" =>5,
+        "name" =>"johnsnow",
+        "designation" =>"Driver",
+        "salary" =>52000
     ],
     
 ];
 
 echo "<table border='2px' cellpadding='5px' cellspacing='0'>
 <tr>
+    <th> id </th>
     <th>name</th>
-    <th>physics</th>
-    <th>maths</th>
-    <th>chemistry</th>
-    
+    <th>designation</th>
+    <th>salary</th>
+
 </tr>";
-foreach($marks as $key =>$v1){
-    echo "<tr>
-          <td>$key</td>";
-         foreach($v1 as $v2){
-             echo "<td> $v2 </td>"; 
-         }
-    echo "</tr>";
+foreach($emp as list("id" =>$id,"name" =>$name,"designation" =>$designation,"salary" =>$salary)){
+    echo "<tr><td>$id</td><td> $name</td> <td>$designation</td> <td>$salary</td> </tr>";
 }
 echo "</table>";
-
-echo "<pre>";
-print_r($marks);
-echo "</pre>";
 
 ?>
