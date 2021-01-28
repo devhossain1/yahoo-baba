b04bb987b55a4e8af6f8be40046c5b472c6cbb17
hossain377 <hossainahmed377@gmail.com>
2021-01-28 09:17:23 +0600

    count $ sizeof

--- index.php
+++ index.php
@@ -1,57 +1,24 @@
 <?php
 
-$emp = [
-    [1,"krisna","Manager",50000],
-    [2,"salman","salesman",20000],
-    [3,"mohon","computer operator",21000],
-    [4,"amir","Driver",5000]
-];
+$food=array("orange","apple","apple","banana","grapes");
 
-$emp =[
-    [
-        "id" =>1,
-        "name" =>"krisna",
-        "designation" =>"manager",
-        "salary" =>50000
-    ],
-     [
-        "id" =>2,
-        "name" =>"Salman",
-        "designation" =>"Salesman",
-        "salary" =>40000
-    ],
-     [
-        "id" =>3,
-        "name" =>"mohon",
-        "designation" => "computer operator",
-        "salary" =>35000
-    ],
-     [
-        "id" =>4,
-        "name" =>"amir",
-        "designation" =>"Driver",
-        "salary" =>5000
-    ],
-    [
-        "id" =>5,
-        "name" =>"johnsnow",
-        "designation" =>"Driver",
-        "salary" =>52000
-    ],
-    
-];
 
-echo "<table border='2px' cellpadding='5px' cellspacing='0'>
-<tr>
-    <th> id </th>
-    <th>name</th>
-    <th>designation</th>
-    <th>salary</th>
 
-</tr>";
-foreach($emp as list("id" =>$id,"name" =>$name,"designation" =>$designation,"salary" =>$salary)){
-    echo "<tr><td>$id</td><td> $name</td> <td>$designation</td> <td>$salary</td> </tr>";
-}
-echo "</table>";
+//$food = array(
+//            'fruits'=> array('orange', 'banana', 'apple'),
+//            'veggie' => array('carrot','collard','pea')
+//        );
+
+//echo count($food['fruits'],1);
+
+//$len= count($food);
+//
+//for($i= 0; $i < $len; $i++){
+//    echo $food[$i] . "<br>";
+//}
+
+echo "<pre>";
+print_r(array_count_values($food));
+echo "</pre>";
 
 ?>
